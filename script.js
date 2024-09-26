fa0da6a36649bd904ba19581e1f2bee3facfb57a
irdepla <rixsiboyevislombek@gmail.com>
2024-09-26 16:54:22 +0500

    Just started

---
+++ script.js
@@ -0,0 +1,48 @@
+// let a = parseFloat(prompt("Summani kiriting: "))
+// let b = parseFloat(prompt("Enter value for b: "))
+// if (b == 1) {
+//     let result = a / 11000
+//     alert(result)
+// }
+// else if(b == 2){
+//     let result = a / 12000
+//     alert(result)
+// }
+// else if (b == 3){
+//     let result = a / 150
+//     alert(result)
+// }
+// else if (b == 4){
+//     let result = a / 90
+//     alert(result)
+// }
+// else if (b == 5){
+//     let result = a / 14000
+//     alert(result)
+// }
+// else{
+//     alert("Noto'g'ri tanlov")
+// }
+
+
+// let n = +prompt("Enter value for n")
+// switch (n){
+//     case 1:
+//         alert("Juda Yomon");
+//         break;
+//     case 2:
+//         alert("Yomon");
+//         break;
+//     case 3:
+//         alert("Qoniqarli");
+//         break;
+//     case 4:
+//         alert("Yaxshi");
+//         break;
+//     case 5:
+//         alert("A'lo");
+//         break;
+//         default:
+//             alert("Noto'g'ri baho");
+//             break;
+// }
